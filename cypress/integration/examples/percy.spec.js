f5d937f4ae4fc79902725a5d58fb15bbcdbeedd1
nerdy-glasses-tester <testergeeknerd@gmail.com>
2021-04-11 12:45:57 -0700

    updated

--- cypress/integration/examples/percy.spec.js
+++ cypress/integration/examples/percy.spec.js
@@ -2,6 +2,6 @@ describe('Visual Regression Testing with Percy and Cypress', () => {
     it('should take percy snapshot', () => {
         cy.visit('http://www.example.com')
         cy.wait(1000)
-        cy.percySnapshot()
+        cy.percySnapshot("My Home")
     })
 })
