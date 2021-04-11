e938ed589d0f4e9ed1006b96b75418121560c167
nerdy-glasses-tester <testergeeknerd@gmail.com>
2021-04-10 20:32:32 -0700

    updated

---
+++ cypress/integration/examples/image.spec.js
@@ -0,0 +1,34 @@
+//import '../../support/commands'
+
+const pages = ["http://www.example.com"]
+
+const sizes = ["iphone-6", "ipad-2", [1200, 800]]
+
+describe('Visual Regression', () => {
+        //cy.visit("https://example.com")
+        //cy.matchImageSnapshot()
+
+        sizes.forEach((size) => {
+            pages.forEach((page) => {
+                it(`Should match ${page} in resolution ${size}`, () => {
+                    let currentTime = new Date(Date.UTC(2020, 1, 1)).getDate()
+                    cy.clock(currentTime)
+                    cy.setResolution(size)
+                    cy.visit(page)
+                    cy.matchImageSnapshot()
+                })
+        })
+    })
+})
+
+describe("Single Element Snapshot", () => {
+    it("Should match a single element on the page", () => {
+        cy.visit("http://example.com")
+        cy.get('h1').matchImageSnapshot(
+            {
+                failureThreshold: 10.0,
+                failureThresholdType: "pixels"
+            }
+        )
+    })
+})
